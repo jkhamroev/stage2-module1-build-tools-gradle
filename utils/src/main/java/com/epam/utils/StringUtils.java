466a02254925d72f00ff3bfd15976ed7962875cc
Khamroev <khamroevjs@gmail.com>
2023-06-21 17:16:25 +0300

    fix

--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -1,7 +1,13 @@
 package com.epam.utils;
 
+import org.apache.commons.lang3.math.NumberUtils;
+
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
-        return org.apache.commons.lang3.StringUtils.isNumeric(str) && Integer.parseInt(str) > 0;
+        try {
+            return NumberUtils.createDouble(str) > 0;
+        } catch (NumberFormatException e) {
+            return false;
+        }
     }
 }
